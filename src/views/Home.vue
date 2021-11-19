34c070aab5a099b20cb6fceabb46611781c44ff0
Gail Eddy <gaileddy68@gmail.com>
2021-11-19 10:42:36 -0600

    adds actions to front end home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+
+    <h1>New Place</h1>
+    Name:
+    <input type="text" v-model="newNameParams.name" />
+    Address:
+    <input type="text" v-model="newAddressParams.address" />
+    <dialog id="place-details">
+      <form method="dialog">
+        <h1>Place Info:</h1>
+        <p>
+          Name:
+          <input type="text" v-model="currentPlace.name" />
+        </p>
+        <p>
+          Address:
+          <input type="text" v-model="currentPlace.address" />
+        </p>
+        <button v-on:click="updatePlace(currentPlace)">Update Place</button>
+        <button v-on:click="destroyPlace(currentPlace)">Delete</button>
+        <button>close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style>
+img {
+  width: 250px;
+}
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Places that have Beer!!",
+      places: [],
+      newPlaceParams: {},
+      currentPlace: {},
+    };
+  },
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("/places").then((response) => {
+        this.places = response.data;
+        console.log("All places", this.places);
+      });
+    },
+    createPlace: function () {
+      console.log("Showing a new place!");
+      axios
+        .post("/places", this.newPlaceParams)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.places.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    showPlace: function (place) {
+      console.log(place);
+      this.currentPlace = place;
+      document.querySelector("#place-details").showModal();
+    },
+    updatePlace: function (place) {
+      axios.patch("/places/" + place.id, place).then((response) => {
+        console.log("Success", response.data);
+      });
+    },
+    destroyPlace: function (place) {
+      axios.delete("/places/" + place.id).then((response) => {
+        console.log("Success!", response.data);
+        var index = this.places.indexOf(place);
+        this.places.splice(index, 1);
+      });
+    },
   },
 };
 </script>
